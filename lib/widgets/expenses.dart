3242a2b04d647d3d453a48fe9b13cc84551bb274
Kakmusaya <scadaengineercs@gmail.com>
2024-05-12 09:49:13 +0500

    added chart widgets

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker_2024/widgets/chart/chart.dart';
 import 'package:expense_tracker_2024/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker_2024/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
@@ -91,7 +92,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text("Char"),
+          Chart(expenses: _registeredExpenses),
           Expanded(
             child: mainContent,
           ),
